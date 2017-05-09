22098d6663553a10baf435f70f1e1783c588596d
Erlemar <and-lukyane@yandex.ru>
2017-05-09 16:57:10 +0300

    RNN and LSTM
    RNN and LSTM

--- assignment3/cs231n/image_utils.py
+++ assignment3/cs231n/image_utils.py
@@ -80,19 +80,20 @@ def deprocess_image(img, mean_img, mean='image', renorm=False):
 
 
 def image_from_url(url):
-  """
-  Read an image from a URL. Returns a numpy array with the pixel data.
-  We write the image to a temporary file then read it back. Kinda gross.
-  """
-  try:
-    f = urllib2.urlopen(url)
-    _, fname = tempfile.mkstemp()
-    with open(fname, 'wb') as ff:
-      ff.write(f.read())
-    img = imread(fname)
-    os.remove(fname)
-    return img
-  except urllib2.URLError as e:
-    print 'URL Error: ', e.reason, url
-  except urllib2.HTTPError as e:
-    print 'HTTP Error: ', e.code, url
+	"""
+	Read an image from a URL. Returns a numpy array with the pixel data.
+	We write the image to a temporary file then read it back. Kinda gross.
+	"""
+	try:
+		f = urllib2.urlopen(url)
+		fd, fname = tempfile.mkstemp()
+		with open(fname, 'wb') as ff:
+			ff.write(f.read())
+		img = imread(fname)
+		os.close(fd)
+		os.remove(fname)
+		return img
+	except urllib2.URLError as e:
+		print 'URL Error: ', e.reason, url
+	except urllib2.HTTPError as e:
+		print 'HTTP Error: ', e.code, url
